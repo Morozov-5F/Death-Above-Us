1e1d83d3fde00e30a246da453f5d13b805cd51b4
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2015-11-07 21:16:03 +0300

    Переписан фон, движение камеры
    - Теперь движется, камера, а не фон
    - Главный фон теперь находится на основном слое
    - Упорядочены слои фона
    - Движение фона подстраивается под размер экрана

---
+++ DeathAboveUs/Assets/scripts/GameScripts/BackgroundLayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundLayer : MonoBehaviour
+{
+    /* Смещение положения слоя относительно камеры
+        0 - слой неподвижен
+        1 - слой движется вместе с камерой
+    */
+    public float layerOffset = 0f;
+    
+    // Положение слоя обновляется в LateUpdate, чтобы слой плавно следовал за камерой
+	void LateUpdate()
+    {
+        float cameraX = Camera.main.transform.position.x;
+        var currentPositon = transform.localPosition;
+        currentPositon.x = cameraX * layerOffset;
+        transform.localPosition = new Vector3(currentPositon.x, currentPositon.y, currentPositon.z);
+    }
+}
---
+++ DeathAboveUs/Assets/scripts/GameScripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+    private PlayerController playerTurret;
+    private float maxPositionOffset;
+    private const float backgroundLayerWidth = 4f;
+
+    void Start () {
+        var playerGameObject = GameObject.Find("Player");
+        playerTurret = playerGameObject.transform.Find("turret").GetComponent<PlayerController>();
+
+        // Вычисление максимального смещения камеры по X
+        float cameraWidth = Camera.main.orthographicSize * 2f * Camera.main.aspect;
+        maxPositionOffset = Mathf.Max(0f, (backgroundLayerWidth - cameraWidth) / 2f);
+    }
+
+	void Update () {
+        // Вычисления поворота турели
+        float turretRotationOffset = playerTurret.transform.localEulerAngles.z;
+        if (turretRotationOffset > 180)
+            turretRotationOffset -= 360;
+        turretRotationOffset /= playerTurret.MAX_ANGLE;
+           
+        // Смещение камеры
+        var currentPositon = transform.localPosition;
+        currentPositon.x = -turretRotationOffset * maxPositionOffset;
+        transform.localPosition = new Vector3(currentPositon.x, currentPositon.y, currentPositon.z);
+    }
+}
