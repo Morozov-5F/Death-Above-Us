3c9dda62f261fdc78258d34627a6d13e151d1980
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2015-11-07 21:53:58 +0300

    Управление турелью игрока
    - Отдельный код для управления тачем

--- DeathAboveUs/Assets/scripts/GameScripts/PlayerController.cs
+++ DeathAboveUs/Assets/scripts/GameScripts/PlayerController.cs
@@ -3,29 +3,63 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour 
 {
-    private Vector3 dragOrigin;
     public float MAX_ANGLE = 65f;
+
+    // Предыдущее положение мыши
+    private Vector3 dragOrigin;
+    private int firstTouchID = -1;
 	
 	void Start () 
 	{
+        #if UNITY_EDITOR
         Input.simulateMouseWithTouches = true;
-	}
+        #else
+        Input.simulateMouseWithTouches = false;
+        #endif
+    }
+
+    void Rotate(float angle)
+    {
+        var currentRotation = transform.localEulerAngles.z;
+        currentRotation += angle;
+        currentRotation = Mathf.Clamp(currentRotation > 180 ? currentRotation - 360 : currentRotation, -MAX_ANGLE, MAX_ANGLE);
+        transform.localEulerAngles = new Vector3(0f, 0f, currentRotation);
+    }
 	
 	void Update () 
 	{
-        if (Input.GetMouseButtonDown(0))
-        {
-            dragOrigin = Input.mousePosition;
-        }
-        else if (Input.GetMouseButton(0))
-        {
-            var dragDelta = dragOrigin - Input.mousePosition;
-            dragOrigin = Input.mousePosition;
+        float inputDeltaX = 0f;
+        // Управление мышью только в редакторе
+        #if UNITY_EDITOR
+            if (Input.GetMouseButtonDown(0))
+            {
+                dragOrigin = Input.mousePosition;
+            }
+            else if (Input.GetMouseButton(0))
+            {
+                var dragDelta = dragOrigin - Input.mousePosition;
+                dragOrigin = Input.mousePosition;
 
-            var currentRotation = transform.localEulerAngles.z;
-            currentRotation += dragDelta.x;
-            currentRotation = Mathf.Clamp(currentRotation > 180 ? currentRotation - 360 : currentRotation, -MAX_ANGLE, MAX_ANGLE);
-            transform.localEulerAngles = new Vector3(0f, 0f, currentRotation);
-        }
-	}
+                inputDeltaX = dragDelta.x;
+            }
+        #else
+        foreach (var touch in Input.touches)
+        {
+            if (firstTouchID >= 0)
+            {
+                if (touch.fingerId == firstTouchID)
+                {
+                    inputDeltaX = -touch.deltaPosition.x;
+
+                    if (touch.phase == TouchPhase.Ended)
+                        firstTouchID = -1;
+                }
+            }
+            else if (touch.phase == TouchPhase.Began)
+                    firstTouchID = touch.fingerId;
+        }         
+        #endif
+
+        Rotate(inputDeltaX);
+    }
 }
