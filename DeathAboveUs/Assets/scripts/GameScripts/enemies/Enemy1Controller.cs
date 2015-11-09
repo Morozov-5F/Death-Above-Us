f8bf757b6a335e591c56f492bfaeffa2207d46e0
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2015-11-09 23:38:08 +0300

    Тестовый противник

---
+++ DeathAboveUs/Assets/scripts/GameScripts/enemies/Enemy1Controller.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy1Controller : Enemy {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Time.deltaTime * 0.1f, 0f, 0f);
+        transform.rotation = Quaternion.Euler(0f, 0f, Mathf.Sin(Time.time * 2f) * 5f);
+	}
+}
