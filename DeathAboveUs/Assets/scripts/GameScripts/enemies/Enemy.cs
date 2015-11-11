da93c8c5f58435c6c7246bc11d9c92c2311bc00b
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2015-11-11 22:52:59 +0300

    Доработка противника
    - Дым
    - Взрыв
    - Удаление

--- DeathAboveUs/Assets/scripts/GameScripts/enemies/Enemy.cs
+++ DeathAboveUs/Assets/scripts/GameScripts/enemies/Enemy.cs
@@ -2,20 +2,49 @@
 using System.Collections;
 
 public class Enemy : MonoBehaviour {
-    public float health;
+    public GameObject explosionPrefab;
+    public float MAX_HEALTH = 10000f;
+    protected float health;
+
+    // Smoke
+    private ParticleSystem smokeEmitter;
+    public float MAX_SMOKE_RATE = 10;
 
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        var smokeObject = transform.Find("Engine Smoke");
+        if (smokeObject)
+        {
+            smokeEmitter = smokeObject.GetComponent<ParticleSystem>();
+        }
+        if (smokeEmitter)
+        {
+            smokeEmitter.Play();
+            smokeEmitter.emissionRate = 0;
+        }
 
-    public void OnCollision()
+        health = MAX_HEALTH;
+    }
+
+    void Explode()
     {
+        // Создание взрыва
+        if (explosionPrefab != null)
+            Instantiate(explosionPrefab, transform.position, transform.rotation);
+    }
 
+    public void OnCollision(float damage)
+    {
+        health = Mathf.Max(0f, health - damage);
+        if (smokeEmitter)
+        {
+            smokeEmitter.emissionRate = MAX_SMOKE_RATE * (1 - health / MAX_HEALTH);
+        }
+        if (health <= 0)
+        {
+            Explode();
+            // Удаление
+            Destroy(gameObject);
+        }
     }
 }
--- DeathAboveUs/Assets/scripts/GameScripts/enemies/Enemy1Controller.cs
+++ DeathAboveUs/Assets/scripts/GameScripts/enemies/Enemy1Controller.cs
@@ -2,11 +2,6 @@
 using System.Collections;
 
 public class Enemy1Controller : Enemy {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
 	
 	// Update is called once per frame
 	void Update () {
